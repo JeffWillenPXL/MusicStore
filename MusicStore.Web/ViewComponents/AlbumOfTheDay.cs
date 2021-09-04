09c776ac1fae3a8b7a6fac6c9a6df5ebf394fd49
Jeff Willen <jeffwillen92@gmail.com>
2021-09-04 19:11:58 +0200

    Add _Album partial view and AlbumOfTheDay viewcomponent

---
+++ MusicStore.Web/ViewComponents/AlbumOfTheDay.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicStore.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicStore.Web.ViewComponents
+{
+    public class AlbumOfTheDay: ViewComponent
+    {
+        private readonly AlbumViewModel _albumViewModel;
+
+        public AlbumOfTheDay()
+        {
+            _albumViewModel = new AlbumViewModel
+            {
+                Artist = "Lady Gaga",
+                Genre = "Pop",
+                Title = "Pokerface"
+            };
+
+
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View("Default",_albumViewModel);
+        }
+    }
+}
